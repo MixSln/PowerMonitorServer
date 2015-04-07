a126855108290bf319ae59b9ccf9eae6d8a9fce6
MixSln <ycm99871@sina.com>
2015-04-07 20:34:44 +0800

    home

---
+++ ServerAil/IPowerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+
+namespace PowerServer.ServerAil
+{
+    [ServiceContract(Namespace="PowerServer")]
+    interface IPowerService
+    {
+        [OperationContract]
+        bool checkUser(string username,string passwd);
+    }
+}
---
+++ ServerAil/PowerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+using PowerServer.ServerBll;
+
+namespace PowerServer.ServerAil
+{
+    [ServiceBehavior(ConcurrencyMode=ConcurrencyMode.Multiple,InstanceContextMode=InstanceContextMode.Single)]
+    class PowerService:IPowerService
+    {
+        #region Service Start
+        protected ServiceHost host = null;
+        public void startService()
+        {
+
+        }
+        #endregion
+
+        #region Implement Interface
+        /// <summary>
+        /// user login check
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="passwd"></param>
+        /// <returns></returns>
+        public bool checkUser(string username, string passwd)
+        {
+            return false;
+        }
+        #endregion
+    }
+}
